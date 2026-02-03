08549457e6f4c164747f40d764ef230ee1ea7ab5
Mayur <redekarm80@gmail.com>
2026-02-03 19:28:19 +0530

    changes

---
+++ app.js
@@ -0,0 +1,15 @@
+// import express from "express";
+
+// const app = express();
+// const PORT = process.env.PORT ?? 8080;
+
+
+// app.get("/", (req, res)=>{
+//   res.send("Hello from Express inside Docker 🚀")
+// })
+
+
+// app.listen(PORT, ()=>{
+//   console.log("App is listening on port 3000");
+  
+// })
